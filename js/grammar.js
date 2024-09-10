11984dc8c149cb0f70443b36e7c45eb977708129
crissipires <crissiano.silva@useall365.onmicrosoft.com>
2024-09-10 00:05:26 -0300

    first commit

---
+++ js/grammar.js
@@ -0,0 +1,32 @@
+export default class Grammar {
+    constructor(nonTerminals, terminals, productions, startSymbol) {
+      this.nonTerminals = new Set(nonTerminals);
+      this.terminals = new Set(terminals);
+      this.productions = productions; 
+      this.startSymbol = startSymbol;
+    }
+ 
+    static deriveSentence(grammar) {
+        let stack = [];
+        let output = '';
+    
+        stack.push(grammar.startSymbol);
+    
+        while (stack.length > 0) {
+            let top = stack.pop(); 
+        
+            if (grammar.terminals.has(top)) {
+                output += top;
+            } else if (grammar.nonTerminals.has(top)) {
+                let productions = grammar.productions[top];
+                let randomProduction = productions[Math.floor(Math.random() * productions.length)];
+
+                for (let i = randomProduction.length - 1; i >= 0; i--) {
+                    stack.push(randomProduction[i]);
+                }
+            }
+        }
+    
+        return output;
+    } 
+}
