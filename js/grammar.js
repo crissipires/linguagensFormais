d05c2e34f73c7cacc5d0a67ecf8131e2e79b39e4
camile.alano <camile.alano@betha.com.br>
2024-09-11 19:54:39 -0300

    correção

--- js/grammar.js
+++ js/grammar.js
@@ -1,33 +1,38 @@
-export default class Gramatica {
+export default class Grammar {
     constructor(naoTerminais, terminais, producoes, simboloInicial) {
-      this.naoTerminais = new Set(naoTerminais);
-      this.terminais = new Set(terminais);
-      this.producoes = producoes; 
-      this.simboloInicial = simboloInicial;
+        this.naoTerminais = new Set(naoTerminais);
+        this.terminais = new Set(terminais);
+        this.producoes = producoes; 
+        this.simboloInicial = simboloInicial;
     }
- 
-    derivarSentenca(gramatica) {
+
+    derivarSentenca() {
         let pilha = [];
         let saida = '';
-    
-        pilha.push(gramatica.simboloInicial);
-    
+
+        pilha.push(this.simboloInicial);
+
         while (pilha.length > 0) {
             let topo = pilha.pop(); 
-        
-            if (gramatica.terminais.has(topo)) {
-                saida += topo;
-            } else if (gramatica.naoTerminais.has(topo)) {
-                let producoes = gramatica.producoes[topo];
-                let producaoAleatoria = producoes[Math.floor(Math.random() * producoes.length)];
 
-                  // é colocado a produção escolhida na pilha, de trás para frente, isso garante que o símbolo mais à esquerda fique no topo
+            if (this.terminais.has(topo)) {
+                saida += topo;
+            } else if (this.naoTerminais.has(topo)) {
+                let producoes = this.producoes[topo];
+                if (!producoes) continue;
+                
+                let producaoAleatoria = producoes[Math.floor(Math.random() * producoes.length)];
+                if (producaoAleatoria === 'e') {
+                    continue; 
+                }
+
+               
                 for (let i = producaoAleatoria.length - 1; i >= 0; i--) {
                     pilha.push(producaoAleatoria[i]);
                 }
             }
         }
-    
+
         return saida;
     } 
 }
